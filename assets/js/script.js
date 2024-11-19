d29a054e3b8f4ab557a7ad1a4088bec3872f8e8f
Unk171 <7353687@gmail.com>
2024-11-18 21:35:01 -0500

    comp answer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const rock = document.getElementById("play1");
 const paper = document.getElementById("play2");
 const scissors = document.getElementById("play3");
 const reset = document.getElementById("reset");
+const compResult = document.getElementById("compChoise");
 const winner = document.getElementById("result")
 const win = document.getElementById("scores1");
 const lose = document.getElementById("scores2");
@@ -10,19 +11,27 @@ let winNum = 0;
 let loseNum = 0;
 let drewNum = 0;
 let result = "";
+let compChoiseMessage = "";
 
 function game() {
     const compChoise = Math.floor(Math.random() * 3);
-
+if (compChoise === 0) {
+    compChoiseMessage = "Rock"
+} else if (compChoise === 1) {
+    compChoiseMessage = "Paper"
+} else {compChoiseMessage = "Scissors"}
     if (result === "rock" && compChoise === 2 || result === "paper" && compChoise === 0 || result === "scissors" && compChoise === 1) {
+        compResult.textContent = `Computer choose ${compChoiseMessage}`
         winner.textContent = "You win!";
         winNum++;
         win.textContent = winNum;
     } else if (result === "rock" && compChoise === 1 || result === "paper" && compChoise === 2 || result === "scissors" && compChoise === 0) {
+        compResult.textContent = `Computer choose ${compChoiseMessage}`
         winner.textContent = "You lost!";
         loseNum++;
         lose.textContent = loseNum;
     } else {
+        compResult.textContent = `Computer choose ${compChoiseMessage}`
         winner.textContent = "Draw!";
         drewNum++;
         draw.textContent = drewNum
